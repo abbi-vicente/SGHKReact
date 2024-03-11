f821bea17bd2d9d6992cda0cf1c61bb388713d7d
Isabel Louise Vicente <isabellouiseph@gmail.com>
2024-03-11 20:02:41 +0800

    SGHKReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const greeting = "Hello React!";
+
+	return (
+		<div>
+			<Navbar />
+			<h1 className="greeting">{greeting}</h1>
+			<p>React is a Javascript library for building user interfaces</p>
+			<About />
+			<Projects />
+		</div>
+	);
+};
 
 export default App;
